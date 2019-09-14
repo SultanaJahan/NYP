141b4694e4ad043fabf6cf29b0d530ebfcc46677
SultanaJahan <sonisultana1454@gmail.com>
2019-09-14 13:35:57 -0400

    test1

--- src/pack1/OpenBrowser.java
+++ src/pack1/OpenBrowser.java
@@ -1,5 +1,15 @@
 package pack1;
 
-public class OpenBrowser {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
+public class OpenBrowser {
+	@Test
+	public void openbrowser(){
+	System.setProperty("webdriver.gecko.driver","C:\\Users\\sonia\\eclipse-workspace\\LearnJava\\Drivers\\geckodriver.exe");
+	WebDriver driver=new FirefoxDriver();
+	driver.get("https://www.facebook.com");
+
+	}
 }
